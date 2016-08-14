149761ae4a8699f52883bbeaf28abf0c9ff09730
Helder <heldrida@gmail.com>
2016-08-14 13:57:19 +0100

    created the first reducer and placed in the reducer combiner

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{	title: 'Javascript: The good parts'	},
+		{	title: 'The Count of Mont Cristo'	},
+		{	title: 'The Dark Tower'	},
+		{	title: 'African jungle'	}
+	]
+}
