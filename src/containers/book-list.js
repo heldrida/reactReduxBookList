6088f2f3baf147738334caa634f6733d17b6289d
Helder <heldrida@gmail.com>
2016-08-14 14:49:00 +0100

    connected action creator to the container book list

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -9,7 +9,12 @@ class BookList extends Component {
 
 		return this.props.books.map((book) => {
 			return (
-				<li key={ book.title } className="list-group-item">{ book.title }</li>
+				<li 
+					key={ book.title } 
+					className="list-group-item" 
+					onClick={ this.props.selectBook(book) }>
+					{ book.title }
+				</li>
 			);
 		});
 
